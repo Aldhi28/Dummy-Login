70729e769bcfa5780e845cc5f3a8605f3c3ca302
Aldhi <aldhiwiduri@gmail.com>
2022-11-27 02:01:10 +0700

    Refactoring an Input Component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={props.onLogout}>Logout</Button>
+      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
   );
 };
